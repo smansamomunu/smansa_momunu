06d245aad3600eb8ce9b82a9cad992346530861f
SMA MOMUNU <smanegeri1momunu@gmail.com>
2020-08-05 18:33:41 +0800

    commit15

--- resources/views/index/materi/index.blade.php
+++ resources/views/index/materi/index.blade.php
@@ -1,8 +1,8 @@
 @extends('index.layouts.app')
 
 @section('content')
-<section class="hero-wrap hero-wrap-2" style="background-image: url('{{asset('index/images/bg_10.jpg')}}');">
-  <div class="overlay"></div>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{asset('index/images/bg_15.jpg')}}');">
+  <div class="vivid"></div>
   <div class="container">
     <div class="row no-gutters slider-text align-items-center justify-content-center">
       <div class="col-md-9 ftco-animate text-center">
@@ -19,16 +19,16 @@
         <p><span>Halaman ini berisi tentang materi-materi dari berbagai mata pelajaran dalam beberapa bentuk termasuk didalamnya:  <b>BUKU, MODUL, TUGAS, </b>dan sebagainya. </span></p>
         <section class="module">
             <div class="module-inner">
-                <div class="side-bar">
+                <div class="side-bar bg-dark">
                     <nav class="side-menu">
                         <ul class="nav">
-                            <li><a href="#"><span class="fa fa-cog"></span> PAI</a></li>
-                            <li><a href="#"><span class="fa fa-cog"></span> PKN</a></li>
-                            <li><a href="#"><span class="fa fa-user"></span> B. Indonesia</a></li>
-                            <li><a href="#"><span class="fa fa-cog"></span> B. Inggris</a></li>
-                            <li><a href="#"><span class="fa fa-cog"></span> Matematika</a></li>
-                            <li><a href="#"><span class="fa fa-cog"></span> Sejarah</a></li>
-                            <li><a href="#"><span class="fa fa-cog"></span> Fisika</a></li>
+                            <li><a href="#"><span class="fa fa-moon-o"></span> PAI</a></li>
+                            <li><a href="#"><span class="fa fa-balance-scale"></span> PKN</a></li>
+                            <li><a href="#"><span class="fa fa-comments"></span> B. Indonesia</a></li>
+                            <li><a href="#"><span class="fa fa-asl-interpreting"></span> B. Inggris</a></li>
+                            <li><a href="#"><span class="fa fa-calculator"></span> Matematika</a></li>
+                            <li><a href="#"><span class="fa fa-history"></span> Sejarah</a></li>
+                            <li><a href="#"><span class="fa fa-"></span> Fisika</a></li>
                             <li><a href="#"><span class="fa fa-cog"></span> Kimia</a></li>
                             <li><a href="#"><span class="fa fa-cog"></span> Biologi</a></li>
                             <li><a href="#"><span class="fa fa-cog"></span> Ekonomi</a></li>
@@ -46,7 +46,7 @@
                             <table class="table">
                                 <thead>
                                     <tr>
-                                        
+
                                         <th class="name truncate">Nama File</th>
                                         <th class="date">Tgl Unggah</th>
                                         <th class="size">Ukuran</th>
@@ -54,7 +54,7 @@
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        
+
                                         <td class="name truncate"><a href="#">Meeting Notes.txt</a></td>
                                         <td class="date">Sep 23, 2015</td>
                                         <td class="size">18 KB</td>
