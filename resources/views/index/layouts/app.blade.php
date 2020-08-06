857cbf73aa609fca4c4ad86170173ddf5aa139be
SMA MOMUNU <smanegeri1momunu@gmail.com>
2020-08-06 14:10:30 +0800

    commit20

--- resources/views/index/layouts/app.blade.php
+++ resources/views/index/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <title>SMA Negeri 1 Momunu</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('index/images/favicon.png') }}"/>
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 
